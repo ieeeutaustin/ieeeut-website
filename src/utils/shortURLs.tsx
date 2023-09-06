7628f2376e243eb2b03f792b0d0b5675b69aaf90
Exium <exium16@gmail.com>
2023-09-05 19:39:53 -0500

    Updated expo shorturl

--- src/utils/shortURLs.tsx
+++ src/utils/shortURLs.tsx
@@ -7,7 +7,7 @@ const shortURLs: any = {
 	officehours: "/assets/images/flyers/Office_Hours_F23.png",
 	gte: "https://docs.google.com/document/d/1H3BMapQV1r5jjtzWx6pEpjJb-CXcKQcgKl--I8qOWdY/edit?usp=sharing",
 	"gm-1": "https://docs.google.com/document/d/1Dj0Z0KZcdj5MAn1SHXBxQa-YntquTGqhTm3z7bM5j1Q/edit",
-	expo: "https://forms.gle/nN4dKPgon8ct4oaE7",
+	expo: "https://drive.google.com/file/d/199XnT_8EY0oekr1TesFqvx_eyRICZO0t/view",
 	"gm-2": "https://forms.gle/8wCtzPKzyhMYoCcD9",
 	"leader-retreat": "https://forms.gle/vmxtQAMKQyJkypSo8",
 	"gm-3": "https://forms.gle/xPAoKseoxT2YJ7qV7",
