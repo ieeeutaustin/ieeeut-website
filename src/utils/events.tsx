12923d75591a721128144370a9fbd2e60cd765de
Exium <exium16@gmail.com>
2024-01-25 16:54:34 -0600

    Added event and shortURL

--- src/utils/events.tsx
+++ src/utils/events.tsx
@@ -6,121 +6,128 @@ import { getShortURL } from "./shortURLs";
 
 const events: any = [
 	{
+		title: "HDR Workshop",
+		desc: "Prepare for expo by networking and mock-interviewing with HDR! 1/25 7pm @ Mulva Foyer",
+		date: "01.25.24",
+		image: "/assets/images/flyers/s24-hdr.jpg",
+		url: getShortURL("hdr")
+	},
+	{
 		title: "Tech Panel (GM #10)",
 		desc: "Learn about tech positions from speakers from Apple, Amazon, and more! 11/09 7pm @ ECJ 1.202",
-		date: "11.09",
+		date: "11.09.23",
 		image: "/assets/images/flyers/f23-gm-10.jpg",
 		url: getShortURL("gm-10")
 	},
 	{
 		title: "Light the Night",
 		desc: "Join a fundraising for Leukemia with IEEE! 11/02 5:30pm - 9:00pm",
-		date: "11.02",
+		date: "11.02.23",
 		image: "/assets/images/flyers/f23-light-the-night.jpg",
 		url: getShortURL("ltn")
 	},
 	{
 		title: "Registration Event (GM #9)",
 		desc: "Course registration help & Freebirds! 10/25 7pm @ ETC 2.136",
-		date: "10.25",
+		date: "10.25.23",
 		image: "/assets/images/flyers/f23-gm-9.jpg",
 		url: getShortURL("gm-9")
 	},
 	{
 		title: "Rock Painting (GM #8)",
 		desc: "De-stress, paint, and snack one some Chi'Lantro! 10/19 7pm @ ETC 2.136",
-		date: "10.19",
+		date: "10.19.23",
 		image: "/assets/images/flyers/f23-gm-8.jpg",
 		url: getShortURL("gm-8")
 	},
 	{
 		title: "HDR Tech Talk",
 		desc: "Meet with HDR and make connections! 10/17 7pm @ Mulva Foyer",
-		date: "10.17",
+		date: "10.17.23",
 		image: "/assets/images/flyers/f23-hdr.jpg",
 		url: getShortURL("hdr")
 	},
 	{
 		title: "Apple Tech Talk (GM #7)",
 		desc: "Listen and network with Apple! 10/12 7pm @ EER 1.518",
-		date: "10.12",
+		date: "10.12.23",
 		image: "/assets/images/flyers/f23-gm-7.jpg",
 		url: getShortURL("gm-7")
 	},
 	{
 		title: "IEEE x WECE (GM #6)",
 		desc: "Join us and WECE for minute to win it games! 10/05 7pm @ ETC 2.136",
-		date: "10.05",
+		date: "10.05.23",
 		image: "/assets/images/flyers/f23-gm-6.jpg",
 		url: getShortURL("gm-6")
 	},
 	{
 		title: "Casino Game Night (GM #5)",
 		desc: "Have fun, play games, and join the raffle! 7-8:30 pm @ EER 3.646",
-		date: "09.28",
+		date: "09.28.23",
 		image: "/assets/images/flyers/f23-gm-5.jpg",
 		url: getShortURL("gm-5")
 	},
 	{
 		title: "Burns & McDonnell Tech Talk (GM #4)",
 		desc: "Learn about Burns & McDonnell and get a chance to network! 7-8 pm @ EER 1.518",
-		date: "09.21",
+		date: "09.21.23",
 		image: "/assets/images/flyers/f23-gm-4.jpg",
 		url: getShortURL("gm-4")
 	},
 	{
 		title: "Optiver Tech Talk (GM #3)",
 		desc: "Listen to Optiver and get a chance to network! 7-8 pm @ EER 1.518",
-		date: "09.14",
+		date: "09.14.23",
 		image: "/assets/images/flyers/f23-gm-3.jpg",
 		url: getShortURL("gm-3")
 	},
 	{
 		title: "Leadership Retreat",
 		desc: "Learn about AO positions and becoming and IEEE officer! 5-8 pm @ Zilker Park",
-		date: "09.08",
+		date: "09.08.23",
 		image: "/assets/images/flyers/f23-leadership-retreat.jpg",
 		url: getShortURL("leader-retreat")
 	},
 	{
 		title: "FamilIEEE Kickoff (GM #2)",
 		desc: "Join and complete with FamilIEEEs! 7-8 pm @ ETC 2.136",
-		date: "09.07",
+		date: "09.07.23",
 		image: "/assets/images/flyers/f23-gm-2.jpg",
 		url: getShortURL("gm-2")
 	},
 	{
 		title: "Expo Prep",
 		desc: "Get ready for Fall Expo with us! 7-8 pm @ ECJ 1.202",
-		date: "09.05",
+		date: "09.05.23",
 		image: "/assets/images/flyers/f23-expo-prep.jpg",
 		url: getShortURL("expo")
 	},
 	{
 		title: "First General Meeting",
 		desc: "Come join us for some Pot Belly! 7-8 pm @ ECJ 1.202",
-		date: "08.31",
+		date: "08.31.23",
 		image: "/assets/images/flyers/f23-gm-1.jpg",
 		url: getShortURL("gm-1")
 	},
 	{
 		title: "Pease Park Litter Pickup",
 		desc: "Volunteer with us to help clean up Pease Park!",
-		date: "04.25",
+		date: "04.25.23",
 		image: "/assets/images/flyers/pease_park.jpg",
 		url: "https://www.instagram.com/p/CrMBlZNu7HS/"
 	},
 	{
 		title: "Boat Social",
 		desc: "End of the year boat social!",
-		date: "04.16",
+		date: "04.16.23",
 		image: "/assets/images/flyers/boat_social.jpeg",
 		url: "https://www.instagram.com/p/CqihT5OLq9C/"
 	},
 	{
 		title: "Elections",
 		desc: "Come vote for your 2023-24 IEEE reps!",
-		date: "04.13",
+		date: "04.13.23",
 		image: "/assets/images/flyers/elections.jpeg",
 		url: "https://www.instagram.com/p/Cq5v660LOCU/"
 	},
@@ -128,28 +135,28 @@ const events: any = [
 	{
 		title: "Cookie BuddIEEEs",
 		desc: "Get some free cookies!",
-		date: "04.11",
+		date: "04.11.23",
 		image: "/assets/images/flyers/Cookies.png",
 		url: "https://www.instagram.com/p/Cqb2-dUL_P0/"
 	},
 	{
 		title: "Paddleboarding",
 		desc: "Join us for some fun paddleboarding!",
-		date: "04.08",
+		date: "04.08.23",
 		image: "/assets/images/flyers/paddleboarding.jpg",
 		url: "https://www.instagram.com/p/CqTF84nLUZK/"
 	},
 	{
 		title: "Registration Tips",
 		desc: "Need help with registration?",
-		date: "04.06",
+		date: "04.06.23",
 		image: "/assets/images/flyers/registration.jpeg",
 		url: "https://www.instagram.com/p/CqYj98YuRE7/"
 	},
 	{
 		title: "IEEE Alumni Panel",
 		desc: "Let's see where our members are now!",
-		date: "03.23",
+		date: "03.23.23",
 		image: "/assets/images/flyers/alumni_panel.jpeg",
 		url: "https://www.instagram.com/p/CqBJCHCrg3r/"
 	}
@@ -164,12 +171,13 @@ export function getUpcoming() {
 	var selected = 0;
 
 	for (var i = 0; i < events.length; i++) {
+		var year = parseInt(events[i].date.split(".")[2]);
 		var month = parseInt(events[i].date.split(".")[0]);
 		var day = parseInt(events[i].date.split(".")[1]);
 
 		// This creates a date at exactly 7pm (CDT) of the event day
 		var eventDate = new Date(
-			Date.UTC(currentDate.getFullYear(), month - 1, day + 1, 0, 0, 0)
+			Date.UTC(year, month - 1, day + 1, 0, 0, 0)
 		);
 
 		if (eventDate.getTime() < currentDate.getTime()) break;
--- src/utils/shortURLs.tsx
+++ src/utils/shortURLs.tsx
@@ -4,27 +4,10 @@
 
 // THESE NEED TO BE IN LOWER CASE (this lets the short url be case INsensitive)
 const shortURLs: any = {
-	officehours: "/assets/images/flyers/Office_Hours_F23.png",
-	gte: "https://docs.google.com/document/d/1H3BMapQV1r5jjtzWx6pEpjJb-CXcKQcgKl--I8qOWdY/edit?usp=sharing",
-	expo: "https://drive.google.com/file/d/199XnT_8EY0oekr1TesFqvx_eyRICZO0t/view",
-	"gm-2": "https://forms.gle/8wCtzPKzyhMYoCcD9",
-	"leader-retreat": "https://forms.gle/vmxtQAMKQyJkypSo8",
-	"gm-3": "https://forms.gle/xPAoKseoxT2YJ7qV7",
-	"gm-4": "https://forms.gle/XesAwtDjfZRtk94v5",
-	"gm-5": "https://forms.gle/Nc9arNyk1FYiriteA",
-	joinafam: "https://forms.gle/NbXRnRtzciX2eeHp6",
-	"gm-6": "https://forms.gle/NojMY7x79V5Tjrho6",
-	"gm-7": "https://forms.gle/fJiiRvhhjNdTPjWe6",
-	hdr: "https://forms.gle/UhS3XSsFqq5ksc6T9",
-	"gm-8": "https://forms.gle/nogbtaMtsEHW8TsS6",
-	ltn: "https://forms.gle/vK8hzA1zQ7uJFmKa7",
-	"gm-9": "https://forms.gle/Ww3uCGLk24hF7o369",
-	"gm-10": "https://forms.gle/o4VmirxZgW6KNSDh7",
-	"arduino": "https://drive.google.com/drive/folders/1NoiDOtpY_jIV9JWX9FAP1PpADzL-Vcxr?usp=drive_link",
-	"famsgiving": "https://forms.gle/X76biJms9jkrp57D8",
-	"slime": "https://forms.gle/eVD8GBCh6RJDU7fY7",
 	"s24-im": "https://docs.google.com/document/d/15DkripHZxPdFSmbZZUM3Bef_IMIYvplAu2oCK3mc88g/edit?usp=sharing",
-	"gm-1": "https://forms.gle/998bY938zhcbzEF38"
+	"hdr": "https://forms.gle/998bY938zhcbzEF38",
+	"gm-1": "https://forms.gle/998bY938zhcbzEF38",
+	"joinafam": "https://docs.google.com/forms/d/e/1FAIpQLSenTEc-6Dt2COxmcLGAwepvuRl9AIwC89o4hUVLvnYbAUSiOg/viewform"
 };
 
 export function getShortURL(name: string) {
