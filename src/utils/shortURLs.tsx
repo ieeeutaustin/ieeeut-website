55fb09f747d3cc756646d3e72673288ab70cbb7e
Exium <exium16@gmail.com>
2024-02-02 18:30:09 -0600

    new shorturl\

--- src/utils/shortURLs.tsx
+++ src/utils/shortURLs.tsx
@@ -7,7 +7,8 @@ const shortURLs: any = {
 	"s24-im": "https://docs.google.com/document/d/15DkripHZxPdFSmbZZUM3Bef_IMIYvplAu2oCK3mc88g/edit?usp=sharing",
 	"hdr": "https://forms.gle/998bY938zhcbzEF38",
 	"gm-1": "https://forms.gle/998bY938zhcbzEF38",
-	"joinafam": "https://docs.google.com/forms/d/e/1FAIpQLSenTEc-6Dt2COxmcLGAwepvuRl9AIwC89o4hUVLvnYbAUSiOg/viewform"
+	"joinafam": "https://docs.google.com/forms/d/e/1FAIpQLSenTEc-6Dt2COxmcLGAwepvuRl9AIwC89o4hUVLvnYbAUSiOg/viewform",
+	"ecjescape": "https://docs.google.com/forms/d/e/1FAIpQLSdC9p8ozE0-6I8MEZBlMgLvvBc7udhvkar7djfrAqVcTyvfoA/viewform?usp=sf_link"
 };
 
 export function getShortURL(name: string) {
