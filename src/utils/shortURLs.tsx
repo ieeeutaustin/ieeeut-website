910e26da3bde79f32433844349210cd60ace08d4
Exium <exium16@gmail.com>
2024-04-01 13:11:30 -0500

    Added shortURL

--- src/utils/shortURLs.tsx
+++ src/utils/shortURLs.tsx
@@ -15,7 +15,8 @@ const shortURLs: any = {
 	"optiver": "https://forms.gle/8eyXShDr9d4Jf2ddA",
 	"climb": "https://forms.gle/EcGoBh26gccEkFqf7",
 	"stars": "https://docs.google.com/forms/d/e/1FAIpQLSej9lcNpVnZCW_sNbQ79ZoxCUQTP7kTF5uqTEK7VpY_0BO4FQ/viewform",
-	"retreat": "https://forms.gle/c5oJLvdpjtjnZKxG6"
+	"retreat": "https://forms.gle/c5oJLvdpjtjnZKxG6",
+	"courses": "https://forms.gle/12vMqvh2ht2ZYB478"
 };
 
 export function getShortURL(name: string) {
